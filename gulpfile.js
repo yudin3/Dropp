e525fa4792caeac33f8ea0b20fce49ffd1eed520
Artem <jazzybagset@gmail.com>
2017-07-27 22:16:17 +0400

    Разбил css и сделал секцию "последние посты"

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-html-minifier');
+var csso = require('gulp-csso');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('minify', function() {
+  gulp.src('')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(''))
+});
+
+gulp.task('default', function () {
+    return gulp.src('')
+        .pipe(csso())
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('development', function () {
+    return gulp.src('')
+        .pipe(csso({
+            restructure: false,
+            sourceMap: true,
+            debug: true
+        }))
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('less', function () {
+    gulp.src('./css/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('./public'));
+});
